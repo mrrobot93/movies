487474c20d4f6a8c53dc0333b91cbd3af7f84a6b
Francisco Cano Poirier-Lalanne <bocaJUNIORS01!>
2025-11-08 19:39:34 +0100

    add provider2

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
-from .providers import CriticAggProvider
+from .providers import CriticAggProvider, AudiencePulseProvider
 
 def main():
     print("Init")
     provider1_path = 'data/provider1.csv'
-    
     critic_agg = CriticAggProvider(file_path=provider1_path)
     critic_data = critic_agg.process()
 
     if critic_data:
         print(f"CriticAgg Rows Processed: {len(critic_data)}")
+
+    provider2_path = 'data/provider2.json'
+    audience_pulse = AudiencePulseProvider(file_path=provider2_path)
+    audience_data = audience_pulse.process()
+
+    if audience_data:
+        print(f"AudiencePulse Rows Processed: {len(audience_data)}")
 
 if __name__ == "__main__":
     main()
